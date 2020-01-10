0031ccc480486dcd5d387a3ed33622bab0132646
alekzonder <alekzonder@gmail.com>
2020-01-10 13:09:44 +0700

    chg: update dev deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "./node_modules/@0devs/package/config/eslint.js"
-};
+    extends: [
+        './node_modules/@0devs/package/config/eslint-ts.js',
+    ],
+    rules: {
+        'import/no-unresolved': 'off',
+        'implicit-arrow-linebreak': ['off'],
+        'import/prefer-default-export': ['off'],
+        '@typescript-eslint/ban-ts-ignore': ['off'],
+    }
+}
